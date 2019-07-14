058fb57418dc32613653c8d4fed06a0d42fdd1f6
Valeriy Mayboroda <valero1@mail.ru>
2019-07-14 19:04:06 +0300

    Add readme file to the project

---
+++ readme.md
@@ -0,0 +1,7 @@
+Test 'http://book.theautomatedtester.co.uk/' site
+* Maven and jdk are already installed
+* Download ChromeDriver from http://chromedriver.chromium.org/
+* Add path to ChromeDriver to environment variables
+* Clone project to user directory
+* Change directory cd ./SeleniumGuide
+* mvn clean test
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -20,13 +20,11 @@ import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
  */
 public abstract class BaseTest {
     private final Logger logger = LoggerFactory.getLogger(BaseTest.class);
-    private String driverPath = "D:/chromedriver/chromedriver.exe";
 
     @BeforeSuite
     public void initWebDriver() {
         browser = "chrome";
         ChromeOptions options = new ChromeOptions();
-        System.setProperty("webdriver.chrome.driver", driverPath);
         options.setExperimentalOption("useAutomationExtension", false);
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("start-maximized");
